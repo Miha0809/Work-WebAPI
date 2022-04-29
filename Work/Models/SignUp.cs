ef5148f482d89a4e86bfb44ad6778b578a20b660
Miha0809 <40777154+Miha0809@users.noreply.github.com>
2022-04-29 15:43:57 +0300

    Change model

--- Work/Models/SignUp.cs
+++ Work/Models/SignUp.cs
@@ -2,8 +2,9 @@ namespace Work.Models;
 
 public class SignUp
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? NameCompany { get; set; }
+    public string FullName { get; set; }
     public string Email { get; set; }
     public string NumberPhone { get; set; }
+    public string Password { get; set; }
 }
